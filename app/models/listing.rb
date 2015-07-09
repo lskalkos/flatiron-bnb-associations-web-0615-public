0e7b742a7ab1ed1bf3b2d6e0ba2ef8cee4d0fe26
Lia Skalkos <hello@liaskalkos.net>
2015-07-09 15:15:22 -0400

    Update Listing and Reservation and Review models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,8 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   belongs_to :neighborhood
   belongs_to :host, :class_name => "User"
+  has_many :guests, through: :reservations
+  has_many :reviews, through: :reservations
+  # delegate :review, to: :reservations
 
 end
